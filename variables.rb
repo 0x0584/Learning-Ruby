8869587d552944b43f083f55f47dfb0328de805c
Anas Rchid <rchid.anas@gmail.com>
2017-04-17 17:58:10 +0100

    Start dealing with variables

---
+++ variables.rb
@@ -0,0 +1,31 @@
+### PART I - Declartion, manipulation
+##
+
+# Declaration
+cool = 8
+pure = 2
+pp = "foo"
+ff = "coco"
+
+# WHY: using cool = 8, pure = 2, pp = "look" is not
+# the same as the one below!!
+# This might be the syntax to declare an Array!!
+
+# Manipulation
+test = cool * pure
+test /= cool
+pure *= cool
+test -= (2/78.0)
+test = pure << cool
+
+# remember:
+# objects separated by ',' has the same 
+# stand-alone execution of the function (i.e. puts)
+
+puts "cool: #{cool} pure: #{pure} pp: #{pp}"
+puts cool, test, "ff:#{ff}"
+puts cool, test; # terminating lines with the classic ';' is fine! YAY!!
+puts pp + ", is just name"
+puts cool * pure
+puts (cool / pure) && (true)
+
