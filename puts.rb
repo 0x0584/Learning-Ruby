dbf0306cf371d57e53fc90f6ea28f3ca32a0b196
Anas Rchid <rchid.anas@gmail.com>
2017-04-17 17:34:27 +0100

    Initial Commit

---
+++ puts.rb
@@ -0,0 +1,25 @@
+### PART I - Normal puts
+##
+
+# puts "TEST!"
+# # puts "This is fun.\n\n"
+# # YAY! comments with the hash. finally!
+# puts "TEST."
+# puts 'TEST! TEST.'
+# puts "test'd 'test'."
+# puts 'test "test" test. .'
+
+### PART II - Extra puts
+##
+
+# puts "test" + "cool", 1+4, 4+3, (3+2 > 5+7),"\ttest\t", 'cool';
+# puts "test",(3+2 || 5+7)
+
+# # WHY: (&4) != (& 4)
+# # puts "1 &4", 1 &4
+# # puts "1 & 4", 1 & 4
+
+# puts "3&2", 3 & 2
+# puts 3 && 2
+# printf "%d\n", 10
+# puts (3+2 != 5+7)
