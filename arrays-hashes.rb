940f55ec309073de7fedb512885def646dda2f68
Anas Rchid <rchid.anas@gmail.com>
2017-04-23 16:07:20 +0100

    add some array manipulations

--- arrays-hashes.rb
+++ arrays-hashes.rb
@@ -2,6 +2,7 @@
 ##
 
 # array of strings
+# str_array = "this", "an", "array", "of", "strings"
 str_array = [ "this", "an", "array", "of", "strings" ]
 puts str_array
 
@@ -9,13 +10,40 @@ puts str_array
 int_array = [1, 5, 6, 4]
 puts int_array
 
+# special initialization
+int_array = Array.new(5){ |i| 2 ** i  }
+puts int_array
+
 # mixed array?
 mixed_array = [1, "bar", 2.3, true]
 puts mixed_array
 
+bool_array = Array.new(4, false)
+puts bool_array
+
+# 2d array
+two_dim = Array.new(3) {  Array.new(4) { |i| 2**i }}
+puts two_dim
+
+# push bool_array into two_dim 
+two_dim << bool_array
+puts two_dim
+
+some_array = [nil, 2, 4, "true", nil, true]
+puts "array"
+puts some_array
+puts "compact"
+# compact return an array with no `nil` elements 
+puts some_array.compact
+puts "compact!"
+# compact! remove all `nil` elements
+puts some_array.compact!
+puts "array"
+puts some_array
+
 ### PART II Hashes
 ##
-
+puts "hashes"
 person = Hash.new               # Hash Class
 
 person["id"] = 10
@@ -28,7 +56,7 @@ puts person["id"].to_s + " " + person["name"]
 person = { :id => 10, :name => "bar"  }
 puts person[:id].to_s + " " + person[:name]
 
-### PART II Arrays and Hashes
+### PART II Collections: Arrays of Hashes
 ##
 
 # long way 
