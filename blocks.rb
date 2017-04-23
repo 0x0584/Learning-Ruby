29c43f704df0e234e9f2aec1b75de0cd065c1029
Anas Rchid <rchid.anas@gmail.com>
2017-04-23 16:08:25 +0100

    starting ruby-blocks

---
+++ blocks.rb
@@ -0,0 +1,29 @@
+### PART I Blocks
+##
+
+i = 0
+count = 5
+
+# similar to for-loop
+count.times do
+  puts (i += 1).to_s
+end
+
+array = [
+  "foo", "bar",
+  "cool", "look"
+]
+
+# loop based on the array's count
+array.each do |foo|
+  puts foo
+end
+
+array = [1, 2, 3, 4]
+
+array.collect! do |n|
+  n ** 2
+end
+
+puts array.inspect
+
