e04fff96b7054dcf754aaf14999f97cd0dd97061
Anas Rchid <rchid.anas@gmail.com>
2017-04-27 20:51:22 +0100

    add: add/remove stuff from a file

--- basics/handle-a-file.rb
+++ basics/handle-a-file.rb
@@ -1,8 +1,25 @@
 ### PART I: get text form a file
-rubyscript = $0                 # the location of the file
-# assign the address of the opened file to some io-typed variable
-content = open(rubyscript)
+##  TODO: understand how this works
 
+rubyscript = $0                 # the location of the file
+content = open(rubyscript)      # assign the address of the file
 puts content.read               # print the content
+
+
+### PART II: write into a file
+##
+
+bak = rubyscript + ".bak"
+file = open(bak, "w")           # open file in write mode
+file.write("# this file was generated" + content.read)
+file.close
+
+### PART II: emptie a file
+##
+
+file = open(bak)           # open file in write mode
+puts file.read
+#file.truncate(0)                # uncomment to take effect
+file.close
 
 content.close                   # always close the opened file!
