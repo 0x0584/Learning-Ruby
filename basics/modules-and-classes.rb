4152066c9f71d86255de70246fceab238041a7ef
Anas Rchid <rchid.anas@gmail.com>
2017-04-29 23:59:34 +0100

    add: initializer

--- basics/modules-and-classes.rb
+++ basics/modules-and-classes.rb
@@ -16,12 +16,22 @@ end
 class Car
   include Vehicle
   @name = ""
-
+  @number = ""
+  @id = ""
+  def initialize (name, number, id = '00')
+    @name = name
+    @number = number
+    @id = id
+  end
   def set(value)
     @name = value
   end
   def get
     @name
+  end
+  # or simply
+  def name= name
+    @name = name
   end
 end
 
@@ -31,3 +41,4 @@ c = Car.new
 puts c.brand("foo").to_s + " " + c.wheels.to_s
 c.set("carr")
 puts c.get
+c.initialize("foo", 10)
