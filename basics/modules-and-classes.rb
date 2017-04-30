2cd154068684b478d786ce3bee31a127dc8efbf2
Anas Rchid <rchid.anas@gmail.com>
2017-05-01 00:47:51 +0100

    add: inheritance between classes, use attr_accessor to get/set

--- basics/modules-and-classes.rb
+++ basics/modules-and-classes.rb
@@ -2,6 +2,7 @@
 ##
 
 module Vehicle                  # or this: Vehicle = Module.new do
+  # methodes
   def wheels
     4
   end
@@ -23,16 +24,19 @@ puts c.brand("foo")
 ##
 
 class Person
+  # private variables
   @name = ""
   @age = 0
   @job = "NONE"
 
+  # constructor
   def initialize(n, a, j = "NONE")
     @name = n
     @age = a
     @job = j
   end
 
+  # set
   def name= name
     @name = name
   end
@@ -42,6 +46,19 @@ class Person
   def job= job
     @job = job
   end
+
+  # get
+  def name
+    @name
+  end
+  def age
+    @age
+  end
+  def job
+    @job
+  end
+
+  # methodes
   def to_str
     "I'm #{@name}, my age is #{@age}, my current job is #{@job}"
   end
@@ -52,3 +69,37 @@ end
 p = Person.new "foo", 20
 
 puts p.to_str
+
+### PART III: Inheritance
+##
+
+class Animal
+  attr_accessor :name           # new way to define get/set
+  
+  def move
+    puts "#{name} is moving.."
+  end
+  def speed
+    10
+  end
+end
+
+class Bird < Animal
+  def fly
+    puts "#{name} can fly too.."
+  end
+  def speed
+    super() * 2
+  end
+end
+
+## TESTING
+
+a = Animal.new
+a.name= "animal"
+a.move
+
+b = Bird.new
+b.name= "bird"
+b.move
+b.fly
