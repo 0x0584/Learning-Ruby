cab9dc6f570852b2409eadb7480ca4f5c066e24b
Anas Rchid <rchid.anas@gmail.com>
2017-05-03 20:25:22 +0100

    add: classic fact() function

---
+++ other/fact.rb
@@ -0,0 +1,16 @@
+# Anas Rchid <rchid.anas@gmail.com>
+# Creation: 05/03/2017
+#
+# About: fact(n)
+# the classic function that computes the factorial of `n`
+
+def fact(n)
+  if n < 0; "negative number"
+  elsif n == 0; 1
+  else; n * fact(n-1)
+  end
+end
+
+## TESTING
+
+puts fact(-3), fact(0), fact(3)  
